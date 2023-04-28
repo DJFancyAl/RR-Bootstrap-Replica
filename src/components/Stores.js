853baeeb39dfd2b73ffd0080e762b24cf5a1035a
DJFancyAl <abluemleiv@gmail.com>
2023-04-28 13:09:40 -0400

    Created a navbar and header

---
+++ src/components/Stores.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+
+function Stores() {
+  return (
+    <Container className='p-3'>
+        <h1>Choose your store near you</h1>
+    </Container>
+  )
+}
+
+export default Stores
